3fc7e12f2c172472deb1bfab6a9449ae78c71b7d
SeeRipStar <110534625+SeeRipStar@users.noreply.github.com>
2023-07-28 09:42:03 +0530

    Update LinearSearch.cpp

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 
 int search(int arr[], int N, int x)
 {
@@ -8,16 +9,14 @@ int search(int arr[], int N, int x)
 	return -1;
 }
 
-
 int main(void)
 {
 	int arr[] = { 2, 3, 4, 10, 40 };
-	int x = 40;
+	int x = 10;
 	int N = sizeof(arr) / sizeof(arr[0]);
-
 	int result = search(arr, N, x);
 	(result == -1)
-		? printf("Element is not present in array")
-		: printf("Element is present at index %d", result);
+		? cout << "Element is not present in array"
+		: cout << "Element is present at index " << result;
 	return 0;
 }
