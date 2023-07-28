d5a5db2b6422d64ebdafdd7b45354d6cf357a449
SeeRipStar <110534625+SeeRipStar@users.noreply.github.com>
2023-07-28 09:40:39 +0530

    Update Bubblesort.cpp

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -1,13 +1,5 @@
-#include <stdbool.h>
-#include <stdio.h>
-
-void swap(int* xp, int* yp)
-{
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
-
+#include <bits/stdc++.h>
+using namespace std;
 
 void bubbleSort(int arr[], int n)
 {
@@ -17,7 +9,7 @@ void bubbleSort(int arr[], int n)
 		swapped = false;
 		for (j = 0; j < n - i - 1; j++) {
 			if (arr[j] > arr[j + 1]) {
-				swap(&arr[j], &arr[j + 1]);
+				swap(arr[j], arr[j + 1]);
 				swapped = true;
 			}
 		}
@@ -31,16 +23,17 @@ void printArray(int arr[], int size)
 {
 	int i;
 	for (i = 0; i < size; i++)
-		printf("%d ", arr[i]);
+		cout << " " << arr[i];
 }
 
 
 int main()
 {
 	int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	bubbleSort(arr, n);
-	printf("Sorted array: \n");
-	printArray(arr, n);
+	int N = sizeof(arr) / sizeof(arr[0]);
+	bubbleSort(arr, N);
+	cout << "Sorted array: \n";
+	printArray(arr, N);
 	return 0;
 }
+
